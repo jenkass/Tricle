45e04464d9b594d1bf289899112d4e311760bf36
Bond <bondarenko.jenek@mail.ru>
2022-03-22 19:10:30 +0300

    Add authentication
    create an authentication function and an endpoint for the login

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from fastapi.staticfiles import StaticFiles
 from db import models
 from db.database import engine
 from router import user, post
+from auth import authentication
 
 app = FastAPI()
 
 
 app.include_router(user.router)
 app.include_router(post.router)
+app.include_router(authentication.router)
 
 
 @app.get("/")
--- router/post.py
+++ router/post.py
@@ -1,13 +1,13 @@
 import random
 import shutil
 import string
-
 from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File
 from sqlalchemy.orm import Session
 
+from auth.oauth2 import get_current_user
 from db import db_post
 from db.database import get_db
-from router.schemas import PostDisplay, PostBase
+from router.schemas import PostDisplay, PostBase, UserAuth
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
@@ -15,7 +15,7 @@ image_url_types = ["absolute", "relative"]
 
 
 @router.post("/create", response_model=PostDisplay)
-def create_post(request: PostBase, db: Session = Depends(get_db)):
+def create_post(request: PostBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
     if request.image_url_type not in image_url_types:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                             detail="Parameter `image_url_types` can only takes values `absolute` or `relative`.")
@@ -28,7 +28,7 @@ def get_all_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/add_image")
-def upload_image(image: UploadFile = File(...)):
+def upload_image(image: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user)):
     letters = string.ascii_letters
     rand_str = ''.join(random.choice(letters) for i in range(6))
     new = f"_{rand_str}."
