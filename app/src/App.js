a98c2c065325a5a809fcdbca3fa0397acd6985ad
Bond <bondarenko.jenek@mail.ru>
2022-03-24 00:04:43 +0300

    Add post component and upload images

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React, {useEffect, useState} from "react";
+import Post from './Post';
 
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL = 'http://localhost:8000/'
 
 function App() {
 
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]); 
 
   useEffect(() =>  {
-    fetch(BASE_URL + "/posts/all")
+    fetch(BASE_URL + "posts/all")
     .then(response => {
       if (response.ok) {
         return response.json()
@@ -24,8 +25,16 @@ function App() {
     })
   }, [])
 
+
+
   return (
-    "hello world!"
+    <div className="app_posts">
+      {
+        posts.map(post => (
+          <Post post={post}/>
+        ))
+      }
+    </div>
   );
 }
 
---
+++ app/src/Post.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from "react";
+import './Post.css';
+
+const BASE_URL = 'http://localhost:8000/'
+
+
+function Post({ post }) {
+
+    const [imageUrl, setImageUrl] = useState('')
+
+    useEffect(() => {
+        if (post.image_url_type == "absolute") {
+            setImageUrl(post.image_url)
+        } else {
+            setImageUrl(BASE_URL + post.image_url)
+        }
+    }, [])
+
+    return (
+        <div className="post">
+            <img className="post_image" src={imageUrl}/>
+        </div>
+    )
+}
+
+export default Post;
