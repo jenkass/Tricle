b2fb562999e3e530e2217b2704ad2b7d5b4786b7
Bond <bondarenko.jenek@mail.ru>
2022-03-23 17:26:04 +0300

    Update React App

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+
+const BASE_URL = 'http://localhost:8000'
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() =>  {
+    fetch(BASE_URL + "/posts/all")
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw response
+    })
+    .then(data => {
+      setPosts(data)
+    })
+    .catch(error => {
+      console.log(error);
+      alert(error)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    "hello world!"
   );
 }
 
